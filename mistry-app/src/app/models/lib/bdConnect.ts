07974607ac15263f9d7923aa32a02e872cc9f6e6
Aryan Khunt <151055762+khuntAryan@users.noreply.github.com>
2024-09-15 11:19:10 +0530

    setting up the Data Base

---
+++ mistry-app/src/app/models/lib/bdConnect.ts
@@ -0,0 +1,29 @@
+import { promises } from "dns"
+import { connect } from "http2"
+import mongoose, { Mongoose } from "mongoose"
+
+type connectionObject = {
+    isConnected?: number
+}
+
+const connection: connectionObject = {}
+
+async function dbConnect(): Promise<void> {
+    if (connection.isConnected) {
+        console.log("already connected to db !!");
+        return
+    }
+
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URI || '', {})
+
+        connection.isConnected = db.connections[0].readyState
+
+        console.log("db connected successfully !!")
+    } catch (error) {
+        console.log("connection failed !!", error);
+        process.exit(1)
+    }
+}
+
+export default dbConnect;
